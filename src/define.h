b3c601c5b5caf54692ba4a6bcc8437ed7bc35247
Meerim Babanova <babanovameerim468@gmail.com>
2026-05-11 15:42:27 +0900

    added a string in defines.h

--- src/define.h
+++ src/define.h
@@ -3,7 +3,6 @@
 // Unsigned int types.
 typedef unsigned char u8;
 typedef unsigned short u16;
-typedef int a;
 typedef unsigned int u32;
 typedef unsigned long long u64;
 
@@ -12,6 +11,7 @@ typedef signed char i8;
 typedef signed short i16;
 typedef signed int i32;
 typedef signed long long i64;
+typedef char* string;
 
 // Floating point types
 typedef float f32;
