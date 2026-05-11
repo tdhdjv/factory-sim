4a58e1b4229ee9e095ec2317846c3be9e2760914
tdhdjv <boseong2007@gmail.com>
2026-05-11 13:36:57 +0900

    Small change in hello.cpp

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-//#include "imgui.h"
+#include "imgui.h"
 
 int main(void) {
-//  std::cout << IMGUI_CHECKVERSION() << std::endl;
+  std::cout << IMGUI_CHECKVERSION() << std::endl;
   std::cout << "Hello World\n" << std::endl;
 }
