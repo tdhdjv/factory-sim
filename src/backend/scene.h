b990fa0b4bf97690ae47f24cc3eea9995a1d61fc
tdhdjv <boseong2007@gmail.com>
2026-05-27 16:00:21 +0900

    Solve Merge conflict

--- src/backend/scene.h
+++ src/backend/scene.h
@@ -1,8 +1,12 @@
-//#include "bac"
+#include "backend/factory.h"
+#include "backend/sink.h"
+#include "backend/source.h"
 
 namespace LongDay {
+	template<class In, class Out>
 	class Scene {
 	private:
+		Factory<
 	public:
 
 	};
