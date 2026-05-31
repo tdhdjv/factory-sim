ca8e374bb230571f791629d7d9a6e3c754dc883d
Meerim Babanova <babanovameerim468@gmail.com>
2026-05-31 14:37:08 +0900

    Solved Merge conflict

--- src/ui/factory_canvas.h
+++ src/ui/factory_canvas.h
@@ -8,13 +8,13 @@ class FactoryCanvas : public LongDay::UI_view {
    private:
    const std::vector<UI::MachineDisplayData>& u_machines;
    const std::vector<UI::ConveyorDisplayData>& u_conveyors;
-   int& u_selectedMachine;
+   i32& u_selectedMachine;
    ImVec2 u_size;
 
    public:
    FactoryCanvas(const std::vector<UI::MachineDisplayData>& machines,
    const std::vector<UI::ConveyorDisplayData>& conveyors,
-   int& selectedMachine, ImVec2 size): u_machines(machines), u_conveyors(conveyors), u_selectedMachine(selectedMachine), u_size(size) {};
+   i32& selectedMachine, ImVec2 size): u_machines(machines), u_conveyors(conveyors), u_selectedMachine(selectedMachine), u_size(size) {};
    
    void draw() override;
 };
